cca312a64813c44d548dbfaaae526e5698e359d7
YuJie Wu <wuvivian725@gmail.com>
2024-11-07 15:12:30 -0800

    setup routing & update app component

--- dreamCatcher/src/app/app-routing.module.ts
+++ dreamCatcher/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {MainpageComponent} from './mainpage/mainpage.component';
+import {TripspageComponent} from './tripspage/tripspage.component';
+import {FavoritelistpageComponent} from  './favoritelistpage/favoritelistpage.component';
+import {UserpageComponent} from  './userpage/userpage.component';
+
+const routes: Routes = [
+  {path: '', component: MainpageComponent},
+  {path: 'user', component: UserpageComponent},
+  {path: 'favlist', component: FavoritelistpageComponent},
+  {path: 'trips', component: TripspageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- dreamCatcher/src/app/app.module.ts
+++ dreamCatcher/src/app/app.module.ts
@@ -4,17 +4,53 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
+import { UserpageComponent } from './userpage/userpage.component';
+import { FavoritelistpageComponent } from './favoritelistpage/favoritelistpage.component';
+import { TripspageComponent } from './tripspage/tripspage.component';
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {DreamCatcherProxyServiceService} from './dream-catcher-proxy-service.service';
+import { provideHttpClient } from '@angular/common/http';
+
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainpageComponent
+    MainpageComponent,
+    UserpageComponent,
+    FavoritelistpageComponent,
+    TripspageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule,
+    MatSortModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatListModule,
+    MatSidenavModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(),
+    DreamCatcherProxyServiceService,
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
