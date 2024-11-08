21fdbdf6ebc6342981cdcd7d9ab6181fda61bd94
YuJie Wu <wuvivian725@gmail.com>
2024-11-07 18:48:09 -0800

    update mainpage

--- dreamCatcher/src/app/mainpage/mainpage.component.ts
+++ dreamCatcher/src/app/mainpage/mainpage.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './mainpage.component.css'
 })
 export class MainpageComponent {
+  locations = [
+    {
+      title: 'Location 1',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      image: 'https://via.placeholder.com/100x100',
+      position: { lat: 64.2559, lng: -21.1295 }
+    },
+    {
+      title: 'Location 2',
+      description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      image: 'https://via.placeholder.com/100x100',
+      position: { lat: 64.3136, lng: -20.2995 }
+    },
+  ];
 
+  ngOnInit() {
+    console.log(this.locations);
+  }
+
+
+  viewLocation(location: any) {
+    console.log('Viewing location:', location.title);
+    // You can add more logic here to display location details
+  }
 }
