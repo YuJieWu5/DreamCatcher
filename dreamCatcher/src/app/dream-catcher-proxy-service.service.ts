4c54ac068252e960de6096d9c3af39251c54f97e
YuJie Wu <wuvivian725@gmail.com>
2024-11-10 13:19:36 -0800

    update frontend service

--- dreamCatcher/src/app/dream-catcher-proxy-service.service.ts
+++ dreamCatcher/src/app/dream-catcher-proxy-service.service.ts
@@ -10,7 +10,70 @@ export class DreamCatcherProxyServiceService {
 
   constructor( private httpClient: HttpClient) { }
 
-  getScenes(){
-    return this.httpClient.get<any[]>(this.hostUrl+'app/scene/');
+  /* API Regarding User*/
+  createAccount(userName: string, phone: number, email: string, password: string){
+    const req = {
+      userId: "",               //will generate userId in backend
+      userName: userName,
+      phone: phone,
+      email: email,
+      password: password,
+      authorization: "general"  //default authorization is general
+    };
+
+    return this.httpClient.post<Record<string, any>>(this.hostUrl+'app/user.json', req);
   }
+
+  getUserInfo(userId: string){
+    return this.httpClient.get<Record<string, any>>(this.hostUrl+'app/user/'+userId+'.json');
+  }
+
+  //updateData only accept userName || phone || email, update user by userId
+  updateUserInfo(updateData: Record<string, any>, userId: string){
+    return this.httpClient.patch<any[]>(this.hostUrl+'app/user/'+userId+'.json', updateData);
+  }
+  
+  /* API Regarding Scene */
+  getScenes(){
+    return this.httpClient.get<Record<string, any>>(this.hostUrl+'app/scene.json');
+  }
+
+  getSceneById(sceneId: string){
+    return this.httpClient.get<Record<string, any>>(this.hostUrl+'app/scene/'+sceneId+'.json');
+  }
+
+  searchScene(keyword: string){
+    return this.httpClient.get<Record<string, any>>(this.hostUrl+'app/scene/search/'+keyword+'.json');
+  }
+
+  /* API Regarding Favorite List */
+  getAllFavoriteList(userId: string){
+    return this.httpClient.get<Record<string, any>>(this.hostUrl+'/app/user/'+userId+'/favoritelist.json');
+  }
+
+  getFavoriteList(userId: string, listId: string){
+    return this.httpClient.get<Record<string, any>>(this.hostUrl+'/app/user/'+userId+'/favoritelist/'+listId+'.json');
+  }
+
+  addSceneToFavoriteList(userId: string, sceneId: string, listId: string){
+    const req = {
+      sceneId: sceneId,
+      listId: listId
+    };
+    const url = this.hostUrl+'/app/user/'+userId+'/addscene.json';
+
+    return this.httpClient.patch<Record<string, any>>(url, req);
+  }
+
+  deleteSceneFromFavoriteList(userId: string, sceneId: string, listId: string){
+    const req = {
+      sceneId: sceneId,
+      listId: listId
+    };
+    const url = this.hostUrl+'/app/user/'+userId+'/deletescene.json';
+
+    return this.httpClient.patch<Record<string, any>>(url, req);
+  }
+
+
 }
