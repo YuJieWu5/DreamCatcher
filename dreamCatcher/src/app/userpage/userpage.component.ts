7548ed271636e1f38a277da7fa9e6af7bf23e649
YuJie Wu <wuvivian725@gmail.com>
2024-11-11 13:27:28 -0800

    Add get user info from server function to userpage

--- dreamCatcher/src/app/userpage/userpage.component.ts
+++ dreamCatcher/src/app/userpage/userpage.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { DreamCatcherProxyServiceService } from '../dream-catcher-proxy-service.service';
+
 
 @Component({
   selector: 'app-userpage',
   templateUrl: './userpage.component.html',
   styleUrl: './userpage.component.css'
 })
-export class UserpageComponent {
 
+export class UserpageComponent {
+  userInfo: Record<string, any> = {}; 
+  userId: string = "a81f83a2-243f-451f-bc4a-fab6538c103d"; //replace to current userId
+
+  constructor(private proxy$: DreamCatcherProxyServiceService){
+    proxy$.getUserInfo(this.userId).subscribe((result: Record<string, any>)=>{
+      console.log(result);
+      this.userInfo = result;
+    });
+  }
+
+  ngOnInit(){
+
+  }
+
+  isObjectEmpty(obj: any): boolean {
+    return !obj || Object.keys(obj).length === 0;
+  }
 }
