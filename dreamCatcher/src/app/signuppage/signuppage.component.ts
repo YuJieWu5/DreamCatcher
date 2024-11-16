b8905b8f01998f7958819e3dc800058bafa487d8
YuJie Wu <wuvivian725@gmail.com>
2024-11-16 01:03:25 -0800

    Add createAccount functionality in signup page

--- dreamCatcher/src/app/signuppage/signuppage.component.ts
+++ dreamCatcher/src/app/signuppage/signuppage.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DreamCatcherProxyServiceService } from '../dream-catcher-proxy-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signuppage',
@@ -12,11 +14,26 @@ export class SignuppageComponent {
   phone: string = "";
   message: string = "";
 
+  constructor(private proxy$: DreamCatcherProxyServiceService, private router: Router){}
+
   validatePhoneInput(event: Event): void {
     const input = event.target as HTMLInputElement;
     // Allow only numeric values
     input.value = input.value.replace(/[^0-9]/g, '');
     this.phone = input.value; // Update the model value
+  }
+
+  createAccount(){
+    this.proxy$.createAccount(this.userName, Number(this.phone), this.email, this.password).subscribe((result: Record<string, any>)=>{
+      console.log(result);
+      const userId = result && result['id'] ? result['id'] : "";
+      if(userId){
+        localStorage.setItem('userId', userId);
+        this.router.navigate(['/']);
+      }else{
+        this.message =result['message'];
+      }
+    })
   }
 
 }
