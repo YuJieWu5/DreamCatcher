7376de6121bdf28654a805e6052df1a6d5000981
YuJie Wu <wuvivian725@gmail.com>
2024-11-16 00:16:27 -0800

    Add API called to userpage

--- dreamCatcher/src/app/userpage/userpage.component.ts
+++ dreamCatcher/src/app/userpage/userpage.component.ts
@@ -10,31 +10,47 @@ export class UserpageComponent {
   userInfo: Record<string, any> = {}; 
   userId: string = "a81f83a2-243f-451f-bc4a-fab6538c103d"; // Replace with the current userId
   isEditing: boolean = false;
+  isLogin: boolean = false;
 
   constructor(private proxy$: DreamCatcherProxyServiceService) {
     proxy$.getUserInfo(this.userId).subscribe((result: Record<string, any>) => {
       console.log('User Info API Response:', result);
-      this.userInfo = {
-        ...result,
-        membershipStatus: this.getMembershipLabel(result['authorization']) // Map authorization to user-friendly label
-      };
+      this.userInfo = result;
+      // this.userInfo = {
+      //   ...result,
+      //   authorization: this.getMembershipLabel(result['authorization'])
+      // };
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    const userIdFromCache = localStorage.getItem('userId');
+    if (userIdFromCache) {
+      this.userId = userIdFromCache;
+      this.isLogin = true;
+    }
+  }
 
   isObjectEmpty(obj: any): boolean {
     return !obj || Object.keys(obj).length === 0;
   }
 
-  toggleEdit() {
+  saveUpdate(){
     if (this.isEditing) {
       // Save logic
-      console.log('Saving:', this.userInfo);
-      this.proxy$.updateUserInfo(this.userInfo, this.userId).subscribe(result => {
+      // console.log('Saving:', this.userInfo);
+      const data = {
+        "userName": this.userInfo['userName'],
+        "phone": this.userInfo['phone']
+      }
+      this.proxy$.updateUserInfo(data, this.userId).subscribe(result => {
         console.log('Save successful:', result);
       });
     }
+    this.isEditing = !this.isEditing;
+  }
+
+  toggleEdit() {
     this.isEditing = !this.isEditing; // Toggle between edit and view mode
   }
 
@@ -49,6 +65,6 @@ export class UserpageComponent {
     if (authorization === 'prime') {
       return 'Premium Member';
     }
-    return 'Normal User';
+    return 'General User';
   }
 }
