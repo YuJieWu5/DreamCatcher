1aa8df267fcb0768ea784e493182f307716564db
only997 <onlylau997@gmail.com>
2024-12-01 16:14:28 -0800

    Fix test for single trip API

--- server/test/testGetSingleTrip.mjs
+++ server/test/testGetSingleTrip.mjs
@@ -6,7 +6,7 @@ chai.use(chaiHttp);
 describe('Test Single Trip result', function () {
     var requestResult;
     var response;
-    const validTripId = "ab0b358d-7b22-4631-93a0-b9d891ddd8f8"; 
+    const validTripId = "7ddc4d5f-d84a-4930-85ba-45c06c"; 
 
     before(function (done) {
         chai.request("http://localhost:8080")
@@ -22,29 +22,29 @@ describe('Test Single Trip result', function () {
 
     it('Should return a valid trip object', function () {
         expect(response).to.have.status(200);
-        expect(response.body).to.be.an('object');
-        expect(response.body).to.have.property('tripId', validTripId);
+        expect(response.body).to.have.property('data').that.is.an('object');
+        expect(response.body.data).to.have.property('tripId', validTripId);
         expect(response).to.have.headers;
     });
-
-
+    
     it('The scenes in the trip should have valid IDs', function () {
-        expect(requestResult.scenes).to.be.an('array');
-        requestResult.scenes.forEach(sceneId => {
-            expect(sceneId).to.be.a('string'); 
+        expect(response.body.data.scenes).to.be.an('array');
+        response.body.data.scenes.forEach(sceneId => {
+            expect(sceneId).to.be.a('string');
         });
     });
-
+    
     it('The trip object has known properties', function () {
-        expect(requestResult).to.include.keys('tripId', 'userId', 'tripName', 'scenes', 'routes');
-        expect(requestResult).to.have.property('tripId').that.is.a('string');
-        expect(requestResult).to.have.property('userId').that.is.a('string');
-        expect(requestResult).to.have.property('tripName').that.is.a('string');
-        expect(requestResult).to.have.property('scenes').that.is.an('array');
-        expect(requestResult).to.have.property('routes').that.is.an('array');
+        expect(response.body.data).to.include.keys('tripId', 'userId', 'tripName', 'scenes', 'routes');
+        expect(response.body.data).to.have.property('tripId').that.is.a('string');
+        expect(response.body.data).to.have.property('userId').that.is.a('string');
+        expect(response.body.data).to.have.property('tripName').that.is.a('string');
+        expect(response.body.data).to.have.property('scenes').that.is.an('array');
+        expect(response.body.data).to.have.property('routes').that.is.an('array');
     });
-
+    
     it('The routes array should be empty or valid', function () {
-        expect(requestResult.routes).to.be.an('array');
+        expect(response.body.data.routes).to.be.an('array');
     });
+    
 });
